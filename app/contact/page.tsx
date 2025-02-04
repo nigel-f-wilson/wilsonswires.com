0e4be9da1883c6c251c73fdc705193c9d0e6cb9d
Nigel Wilson <nigel@stemgarden.org>
2025-02-04 08:09:01 -0600

    Style contact form on large screens

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,18 +15,12 @@ const FormLabel: React.FC<FormLabelProps> = ({ label }) => {
     </span>
   );
 }
-
-interface RadioButtonProps {
-  label: string;
-  selected: boolean;
-} 
-
   
 export default function Page() {
   const [addressesAreSame, setAddressesAreSame] = useState(false);
 
   const formItemClasses = "flex flex-col sm:flex-row mb-4"
-  const inputClasses = "peer block w-full rounded-md border border-gray-200 py-[9px] pl-4 text-sm outline-2 placeholder:text-gray-500"
+  const inputClasses = "peer block w-full rounded-md border border-gray-200 pl-4 text-sm sm:text-lg placeholder:text-gray-500 outline-2 "
   
 
   return (
@@ -86,49 +80,49 @@ export default function Page() {
           <div className='flex items-center'>
             <input type="checkbox" id="check"
               checked={addressesAreSame} 
-              onChange={() => setAddressesAreSame(!addressesAreSame)} 
+              onChange={(e) => setAddressesAreSame(e.target.checked)} 
               data-collapse-target="collapse"
               className="peer h-5 w-5 transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800 mr-4"  />
-              {/* <span className="absolute text-black opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
-                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-                </svg>
-              </span> */}
               <span className="">Billing address is the same as the address where work will be performed.</span>
             </div>
         </label>
 
         {/* BILLING ADDRESS */}
-        <label 
-          data-collapse="collapse"
-          className={`block h-0 w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${formItemClasses}`} 
-        >
-          <FormLabel label="Billing Address:" />
-          <input id="projectAddress" type="projectAddress" name="projectAddress"
-            className={inputClasses}
-            placeholder="Address you want invoice sent to"
-            required
-          />
-        </label>
+        {!addressesAreSame && (
+          <label 
+            data-collapse="collapse"
+            className={`block h-0 w-full basis-full overflow-hidden  ${formItemClasses}`} 
+          >
+            <FormLabel label="Billing Address:" />
+            <input id="projectAddress" type="projectAddress" name="projectAddress"
+              className={inputClasses}
+              placeholder="Address to send the invoice to"
+              required={!addressesAreSame}
+            />
+          </label>
+        )}
 
         
         {/* MESSAGE */}
-        <label className={formItemClasses} >
+        <label className={`${formItemClasses} flex sm:flex-col`} >
           <FormLabel label="What can we do for you?" />
           <textarea 
             name="message" 
-            placeholder="Describe your project. The more detail the better. "
+            placeholder="Describe your project. The more detail the better. We will call you with any further questions."
             required
             className={inputClasses}
           />
         </label>
 
-        <button type="submit" className="mt-4 w-full">
-          Send <EnvelopeIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </button>
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        <div className="flex w-full justify-center" >
+          <button type="submit" className="flex h-10 w-fit  items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50">
+            <EnvelopeIcon className="mr-4 h-5 w-5 text-gray-50 " />
+            Send 
+          </button>
         </div>
+        {/* <div className="flex h-8 items-end space-x-1">
+          Add form errors here
+        </div> */}
       </form>
 
     </main>
